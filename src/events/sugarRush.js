195ff88c82c45bd60547b551b84b8f44c312e030
Jake Siegers <sirtopeia@yahoo.com>
2015-12-31 17:08:09 -0600

    Fixed error function

--- src/events/sugarRush.js
+++ src/events/sugarRush.js
@@ -5,7 +5,7 @@ function SugarRush(TrickOrTreatOBJ,userObj,callback){
 }
 
 SugarRush.prototype.SugarRushResult = function(userObj,callback,error,results,fields){
-	if(error !== null){ this.error(error); return;}
+	if(error !== null){ this.trt.error(error); return;}
 	callback('You just got a sugar rush, allowing you to trick or treat (1) additional time today! (And this one did not count!)');
 };
 
